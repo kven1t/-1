866cc5fac55507c51db8578ddd8661fe846e0644
hp <hp@HP>
2023-02-19 18:30:10 +0300

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace Практика_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnExit = new System.Windows.Forms.Button();
+            this.txtA = new System.Windows.Forms.TextBox();
+            this.txtB = new System.Windows.Forms.TextBox();
+            this.txtC = new System.Windows.Forms.TextBox();
+            this.txtP = new System.Windows.Forms.TextBox();
+            this.btnCount = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(478, 232);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(61, 25);
+            this.btnExit.TabIndex = 0;
+            this.btnExit.Text = "Выход";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // txtA
+            // 
+            this.txtA.Location = new System.Drawing.Point(50, 31);
+            this.txtA.Name = "txtA";
+            this.txtA.Size = new System.Drawing.Size(80, 22);
+            this.txtA.TabIndex = 1;
+            this.txtA.Text = "0";
+            // 
+            // txtB
+            // 
+            this.txtB.Location = new System.Drawing.Point(170, 31);
+            this.txtB.Name = "txtB";
+            this.txtB.Size = new System.Drawing.Size(80, 22);
+            this.txtB.TabIndex = 2;
+            this.txtB.Text = "0";
+            // 
+            // txtC
+            // 
+            this.txtC.Location = new System.Drawing.Point(290, 31);
+            this.txtC.Name = "txtC";
+            this.txtC.Size = new System.Drawing.Size(80, 22);
+            this.txtC.TabIndex = 3;
+            // 
+            // txtP
+            // 
+            this.txtP.Location = new System.Drawing.Point(420, 31);
+            this.txtP.Name = "txtP";
+            this.txtP.Size = new System.Drawing.Size(80, 22);
+            this.txtP.TabIndex = 4;
+            // 
+            // btnCount
+            // 
+            this.btnCount.Location = new System.Drawing.Point(217, 148);
+            this.btnCount.Name = "btnCount";
+            this.btnCount.Size = new System.Drawing.Size(123, 35);
+            this.btnCount.TabIndex = 5;
+            this.btnCount.Text = "Вычислить";
+            this.btnCount.UseVisualStyleBackColor = true;
+            this.btnCount.Click += new System.EventHandler(this.btnCount_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(82, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(16, 16);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "A";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(199, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(16, 16);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "B";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(324, 12);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(16, 16);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "C";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(448, 9);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(16, 16);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "P";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(570, 278);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnCount);
+            this.Controls.Add(this.txtP);
+            this.Controls.Add(this.txtC);
+            this.Controls.Add(this.txtB);
+            this.Controls.Add(this.txtA);
+            this.Controls.Add(this.btnExit);
+            this.Name = "Form1";
+            this.Text = "option 15";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.TextBox txtA;
+        private System.Windows.Forms.TextBox txtB;
+        private System.Windows.Forms.TextBox txtC;
+        private System.Windows.Forms.TextBox txtP;
+        private System.Windows.Forms.Button btnCount;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Практика_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnCount_Click(object sender, EventArgs e)
+        {
+            int D = (Int32.Parse(txtA.Text) * Int32.Parse(txtA.Text)) +
+                (Int32.Parse(txtB.Text) * Int32.Parse(txtB.Text));
+
+            double C = Math.Sqrt(D);
+
+            txtC.Text = C.ToString("0.000");
+
+            Double P = Int32.Parse(txtA.Text) + Int32.Parse(txtB.Text) + C;
+
+            txtP.Text = P.ToString("0.000");
+        }
+    }
+}
